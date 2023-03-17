375a6896567895b8f2c3bddef416bd8c6f26e4f1
Sants <128149229+eoSants@users.noreply.github.com>
2023-03-17 09:41:16 -0300

    Create aluno.java
    projeto inacabado

---
+++ aluno.java
@@ -0,0 +1,15 @@
+public class Aluno{ 
+private String nome;
+private String cpf;
+public void setNome (String nome) {
+this.nome=nome;
+} 
+public String getNome(){
+return nome; 
+}
+public void setCpf (String cpf) {
+this.cpf=cpf;
+} 
+public String getCpf(){
+return Cpf; 
+} 
